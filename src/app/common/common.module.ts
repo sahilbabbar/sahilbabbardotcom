71790e2acb116f4a57a12d3281fb398751030f5a
Sahil Babbar <me@sahilbabbar.com>
2018-05-08 23:47:29 +0530

    Homepage Initiated

---
+++ src/app/common/common.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  imports: [],
+  exports: [
+    NavigationComponent
+  ],
+  declarations: [NavigationComponent]
+})
+export class CommonModule { }
