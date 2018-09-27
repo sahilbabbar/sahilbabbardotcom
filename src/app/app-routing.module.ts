8676e7005f3f1badd0f2c50ed13481e8b60a14b8
Sahil Babbar <sahilbabbarrs@gmail.com>
2018-09-27 20:57:22 +0530

    Minor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: 'app.module#AppModule'
-  },
-  {
-    path: 'home',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+//
+// const routes: Routes = [];
+//
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
