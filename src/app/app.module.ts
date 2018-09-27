dce1924f55971f2b17878d7dd8d2eb8d604d5da8
Sahil Babbar <sahilbabbarrs@gmail.com>
2018-09-27 20:46:01 +0530

    Homepage Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,7 @@ import { PagesModule } from './pages/pages.module';
   ],
   imports: [
     BrowserModule,
-    PagesModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
