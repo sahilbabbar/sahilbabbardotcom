019ec59983f03beaba2dac88b2c8d6ed46ff572b
Sahil Babbar <me@sahilbabbar.com>
2019-12-16 11:29:50 +0530

    feat(navbar): navbar initial commit
    Description: Code moved to a separate component and added some CSS changes.
    Breaking Changes: NULL
    Issue: NA

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -2,13 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './components/home/home.component';
 import {BlogRoutingModule} from './blog-routing.module';
-import { PostComponent } from './components/post/post.component';
+import {PostComponent} from './components/post/post.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        BlogRoutingModule
+        BlogRoutingModule,
+        SharedModule
     ],
     declarations: [HomeComponent, PostComponent]
 })
-export class BlogModule { }
+export class BlogModule {
+}
