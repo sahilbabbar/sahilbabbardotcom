22c890c98f6cd0b5ac5b11a160c972d2e15da525
Sahil Babbar <me@sahilbabbar.com>
2022-05-16 16:56:40 +0530

    chore(*): empty project with angular v13.3.5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,10 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-    {
-        path: '',
-        loadChildren: './modules/bio/bio.module#BioModule'
-    },
-    {
-        path: 'blog/MigratingEnterpriseAngularAppToAnotherDesignSystem',
-        loadChildren: './modules/blog/blog.module#BlogModule'
-    },
-    {
-        path: '**',
-        redirectTo: '',
-        pathMatch: 'full'
-    }
-];
+const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule
-    ],
-    providers: [
-        {provide : LocationStrategy , useClass: HashLocationStrategy}
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
