a6d633fe1d1c0384b43d12745e754d09a3b7cae1
Sahil Babbar <sahilbabbarrr@gmail.com>
2024-12-04 21:10:47 +0530

    feat: adding routes

---
+++ src/app/project/project.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sb-project',
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.scss'
+})
+export class ProjectComponent {
+
+}
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sb-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    standalone: true
+})
+export class HeaderComponent {}
