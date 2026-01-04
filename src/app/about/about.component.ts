7d60503b009a11fe7789784e113028ae05d7a260
Sahil Babbar <sahilbabbarrr@gmail.com>
2026-01-05 00:09:50 +0530

    chore: temporary landing page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'sb-about',
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+    selector: 'sb-about',
+    templateUrl: './about.component.html',
+    standalone: true,
+    imports: [NgOptimizedImage, RouterLink],
+    styleUrl: './about.component.scss'
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
